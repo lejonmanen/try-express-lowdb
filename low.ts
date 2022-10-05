02fb31ebc38eb63cdb98c957f4160703e0791403
David Andersson <david.andersson@zocom.se>
2022-10-05 14:05:10 +0200

    Works with TypeScript now

--- low.ts
+++ low.ts
@@ -1,18 +1,36 @@
 import { join, dirname } from 'path'
-import { Low, JSONFile } from 'lowdb'
 import { fileURLToPath } from 'url'
+// import down_lo from 'lowdb'
+// import lowdb from 'lowdb'
+// import FileSync from "lowdb/adapters/FileSync";
+import { Low, JSONFile } from 'lowdb'
+
+
+type Fruit = string
+
+type Schema = {
+	fruits: Fruit[];
+}
+
+// const adapter = new FileSync<Schema>('db.json')
+// let db: lowdb.LowdbSync<Schema> = lowdb(adapter)
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const file = join(__dirname, 'db.json')
-const adapter = new JSONFile(file)
+const adapter = new JSONFile<Schema>(file)
 const db = new Low(adapter)
 
-await db.read()
+db.read()
+// let fruits = db.get('fruits').value()
+// console.log('fruits', fruits);
+
+// if( !fruits ) {
 if( !db.data || !db.data.fruits ) {
+	// db.defaults({ fruits: ['apple'] })
 	db.data = { fruits: ['apple'] }
-	await db.write()
+	db.write()
 }
-console.log('db', db.data);
+// console.log('db', db.data);
 
 const fruits = db.data.fruits
 // fruits.push('apple')
